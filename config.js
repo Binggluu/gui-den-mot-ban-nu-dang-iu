58ba0a14f6232b433d5c02dd6dd64e3efef31594
Binggluu <87159563+Binggluu@users.noreply.github.com>
2022-06-02 19:06:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bae à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu chứ <33',
+    btnNo: 'Mơ à!!!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Thứ 7 anh đưa đi uống trà sữa nháaa.',
+    btnAccept: 'Em đồng ýyyyy <3',
+ messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
